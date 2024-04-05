4354ca8011f9bdba821b7e83a944e5582651c44a
M0rEe <62582432+M0rEe@users.noreply.github.com>
2024-04-05 13:24:45 +0200

    Added UART Module

---
+++ UART_0/UART_0_interface.h
@@ -0,0 +1,31 @@
+/*
+ * UART_0_interface.h
+ *
+ *  Created on: Apr 5, 2024
+ *      Author: M0rE
+ */
+
+#ifndef UART_0_UART_0_INTERFACE_H_
+#define UART_0_UART_0_INTERFACE_H_
+
+#include  <stdint.h>
+#include  <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "stddef.h"
+
+// - UART0 peripheral - As console to display debug messages.
+//     - UART0RX - PA0
+//     - UART0TX - PA1
+//
+// - UART parameters for the UART0 and UART7 port:
+//     - Baud rate - 115,200
+//     - 8-N-1 operation
+
+void uart_init(void);
+void uart_sendString(uint8_t *ptr);
+
+#endif /* UART_0_UART_0_INTERFACE_H_ */
--- main.c
+++ main.c
@@ -4,25 +4,24 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-#include  "driverlib/systick.h"
-#include  "driverlib/interrupt.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
 
-#include "BitMath.h"
-#include "tm4c123gh6pm.h"
-#include  "inc/hw_nvic.h"
-#include  "inc/hw_types.h"
-#include  "inc/hw_ints.h"
+#include  "BitMath.h"
+#include  "tm4c123gh6pm.h"
 
 #include "Application/Application_interface.h"
 
+#include "UART_0/UART_0_interface.h"
+
 uint32_t SystemCoreClock = 16000000;
-
-
 
 #define BUILT_IN_LED_1  1
 #define BUILT_IN_LED_2  2
 #define BUILT_IN_LED_3  3
-
 
 void vPeriodicTask(void *pvParameters)
 {
@@ -30,6 +29,25 @@ void vPeriodicTask(void *pvParameters)
     // Establish the task's period.
     const TickType_t xDelay = pdMS_TO_TICKS(1000);
     TickType_t xLastWakeTime = xTaskGetTickCount();
+
+    for (;;)
+    {
+
+        Tog_Bit(GPIO_PORTF_DATA_R, BUILT_IN_LED_3);
+
+        uart_sendString("helllllllllo\n");
+
+        // Block until the next release time.
+        //vTaskDelayUntil(&xLastWakeTime, xDelay);
+        vTaskDelay(xDelay);
+    }
+
+}
+
+//----------------------
+//------------
+int main()
+{
 
     // Enable Clock for PORTF
     Set_Bit(SYSCTL_RCGCGPIO_R, 5);
@@ -44,30 +62,16 @@ void vPeriodicTask(void *pvParameters)
 //----------------------
     // Disable alternative function for PF3
     Clr_Bit(GPIO_PORTF_AFSEL_R, BUILT_IN_LED_3);
-
-
 //--------------------
-
     // PF3 as output
     Set_Bit(GPIO_PORTF_DIR_R, BUILT_IN_LED_3);
 
     // PF2 as output
     Set_Bit(GPIO_PORTF_DIR_R, BUILT_IN_LED_2);
 
-    for (;;)
-    {
+//    char cThisChar;
 
-        Tog_Bit(GPIO_PORTF_DATA_R, BUILT_IN_LED_3);
-        // Block until the next release time.
-        //vTaskDelayUntil(&xLastWakeTime, xDelay);
-        vTaskDelay(xDelay);
-    }
-
-}
-
-int main()
-{
-    //IntMasterEnable();
+    uart_init();
 
     xTaskCreate(vPeriodicTask, "My Task", 256, NULL, 1, NULL);
 
